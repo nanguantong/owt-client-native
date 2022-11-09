7f3db90d6650958fd60ccc4e9515deb714b8c26a
nanguantong <zww0602jsj@gmail.com>
2022-11-09 12:53:25 +0800

    fix sio without https

--- talk/owt/sdk/conference/conferencesocketsignalingchannel.cc
+++ talk/owt/sdk/conference/conferencesocketsignalingchannel.cc
@@ -127,7 +127,9 @@ void ConferenceSocketSignalingChannel::Connect(
     }
     return;
   }
-  std::string scheme("http://");
+  bool secure = false;
+  rtc::GetBoolFromJsonObject(json_token, "secure", &secure);
+  std::string scheme(secure ? "https://" : "http://");
   std::string host;
   rtc::GetStringFromJsonObject(json_token, "host", &host);
   std::weak_ptr<ConferenceSocketSignalingChannel> weak_this =
